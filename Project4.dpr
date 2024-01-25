765e0b69a3c7b0b5c0362e4dc0250cf77d2b0054
youngje ahn <civilian7@gmail.com>
2024-01-25 15:07:11 +0900

    Sample projects

---
+++ Project4.dpr
@@ -0,0 +1,14 @@
+﻿program Project4;
+
+uses
+  Vcl.Forms,
+  Unit6 in 'Unit6.pas' {uMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TuMain, uMain);
+  Application.Run;
+end.
---
+++ Unit6.pas
@@ -0,0 +1,36 @@
+﻿unit Unit6;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.Generics.Collections,
+  System.IOUtils,
+
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs;
+
+type
+  TuMain = class(TForm)
+  private
+    procedure GetFiles();
+  public
+    { Public declarations }
+  end;
+
+var
+  uMain: TuMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TuMain.GetFiles;
+begin
+  //var
+end;
+
+end.
